29bbb91d716fccb22f9235a08e62099e69927184
Pa Khan <don@pa-khan.com>
2024-11-09 00:52:18 +0300

    SNIP-106 Создать пакет логгера

--- go.mod
+++ go.mod
@@ -3,15 +3,16 @@ module github.com/budka-tech/logit-go
 go 1.23.1
 
 require (
+	github.com/budka-tech/configo v0.1.2
 	github.com/budka-tech/envo v0.0.1
 	github.com/getsentry/sentry-go v0.29.1
 	github.com/google/uuid v1.6.0
 	go.uber.org/zap v1.27.0
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1
 )
 
 require (
 	github.com/BurntSushi/toml v1.4.0 // indirect
-	github.com/budka-tech/configo v0.0.2 // indirect
 	github.com/ilyakaznacheev/cleanenv v1.5.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
--- logit.go
+++ logit.go
@@ -8,95 +8,168 @@ import (
 	"github.com/google/uuid"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
+	"gopkg.in/natefinch/lumberjack.v2"
+	"os"
+	"path/filepath"
 	"time"
 )
 
-func NewLogger(sen configo.Sentry, env *envo.Env, appVersion string) (*zap.Logger, error) {
-	err := sentry.Init(sentry.ClientOptions{
-		Dsn:              fmt.Sprintf("https://%v@%v", sen.Key, sen.Host),
-		TracesSampleRate: 1.0,
-		Debug:            true,
-		Environment:      env.String(),
-	})
+type LogIt struct {
+	logger *zap.Logger
+}
 
-	if err != nil {
-		panic("Ошибка инициализации Sentry: " + err.Error())
+// Debug - логирование отладочной информации
+func (receiver *LogIt) Debug(op string, traceId *string, fields ...zap.Field) {
+	traceId = receiver.TraceId(traceId)
+	receiver.logger.Debug(
+		"Debug",
+		append([]zap.Field{
+			zap.String("op", op),
+			zap.String("traceId", *traceId),
+		}, fields...)...,
+	)
+}
+
+// Info - логирование информационных сообщений
+func (receiver *LogIt) Info(message, op string, traceId *string, fields ...zap.Field) {
+	traceId = receiver.TraceId(traceId)
+	receiver.logger.Info(
+		message,
+		append([]zap.Field{
+			zap.String("op", op),
+			zap.String("traceId", *traceId),
+		}, fields...)...,
+	)
+}
+
+// Warn - логирование предупреждений
+func (receiver *LogIt) Warn(message, op string, traceId *string, fields ...zap.Field) {
+	traceId = receiver.TraceId(traceId)
+	receiver.logger.Warn(
+		message,
+		append([]zap.Field{
+			zap.String("op", op),
+			zap.String("traceId", *traceId),
+		}, fields...)...,
+	)
+}
+
+// Error - логирование ошибок
+func (receiver *LogIt) Error(err error, op string, traceId *string, fields ...zap.Field) {
+	traceId = receiver.TraceId(traceId)
+	receiver.logger.Error(
+		err.Error(),
+		append([]zap.Field{
+			zap.String("op", op),
+			zap.String("traceId", *traceId),
+		}, fields...)...,
+	)
+	sentry.CaptureException(err)
+}
+
+// Fatal - логирование критических ошибок, завершает приложение
+func (receiver *LogIt) Fatal(err error, op string, traceId *string, fields ...zap.Field) {
+	traceId = receiver.TraceId(traceId)
+	receiver.logger.Fatal(
+		err.Error(),
+		append([]zap.Field{
+			zap.String("op", op),
+			zap.String("traceId", *traceId),
+		}, fields...)...,
+	)
+	sentry.CaptureException(err)
+}
+
+// MustNewLogger - инициализация нового логгера
+func MustNewLogger(appConf *configo.App, loggerConf *configo.Logger, senConf *configo.Sentry, env *envo.Env) *LogIt {
+	encoderConfig := zapcore.EncoderConfig{
+		TimeKey:        "time",
+		LevelKey:       "level",
+		NameKey:        "logger",
+		CallerKey:      "caller",
+		MessageKey:     "msg",
+		StacktraceKey:  "traceId",
+		LineEnding:     zapcore.DefaultLineEnding,
+		EncodeLevel:    zapcore.CapitalColorLevelEncoder,
+		EncodeTime:     zapcore.TimeEncoderOfLayout(loggerConf.TimeFormat),
+		EncodeDuration: zapcore.StringDurationEncoder,
+		EncodeCaller:   zapcore.ShortCallerEncoder,
 	}
 
-	cfg := zap.Config{
-		Level:       zap.NewAtomicLevelAt(zap.DebugLevel),
-		Development: true,
-		Encoding:    "console",
-		EncoderConfig: zapcore.EncoderConfig{
-			TimeKey:        "time",
-			LevelKey:       "level",
-			NameKey:        "op",
-			CallerKey:      "caller",
-			MessageKey:     "msg",
-			StacktraceKey:  "stacktrace",
-			LineEnding:     zapcore.DefaultLineEnding,
-			EncodeLevel:    zapcore.CapitalColorLevelEncoder,
-			EncodeTime:     zapcore.ISO8601TimeEncoder,
-			EncodeDuration: zapcore.StringDurationEncoder,
-			EncodeCaller:   zapcore.ShortCallerEncoder,
-		},
-		OutputPaths:      []string{"stdout"},
-		ErrorOutputPaths: []string{"stderr"},
-	}
+	var encoder zapcore.Encoder
 
-	switch *env {
-	case "prod":
-		cfg = zap.NewProductionConfig()
-		cfg.Level = zap.NewAtomicLevelAt(zap.InfoLevel)
-		cfg.Development = false
-		cfg.Sampling = &zap.SamplingConfig{
-			Initial:    100,
-			Thereafter: 100,
+	if !env.IsLocal() {
+		if senConf != nil {
+			err := sentry.Init(sentry.ClientOptions{
+				Dsn:              fmt.Sprintf("https://%v@%v", senConf.Key, senConf.Host),
+				TracesSampleRate: 1.0,
+				Debug:            true,
+				Environment:      env.String(),
+			})
+
+			if err != nil {
+				panic("Ошибка инициализации Sentry: " + err.Error())
+			}
 		}
-	default:
-		cfg = zap.NewProductionConfig()
-		cfg.EncoderConfig.NameKey = "op"
+		encoder = zapcore.NewJSONEncoder(encoderConfig)
+	} else {
+		encoder = zapcore.NewConsoleEncoder(encoderConfig)
 	}
 
-	logger, err := cfg.Build()
-	if err != nil {
-		return nil, err
+	var cores []zapcore.Core
+
+	if loggerConf.EnableConsole {
+		consoleWriter := zapcore.Lock(os.Stdout)
+		cores = append(cores, zapcore.NewCore(encoder, consoleWriter, zapcore.Level(loggerConf.ConsoleLevel)))
 	}
 
-	logger = logger.With(zap.String("version", appVersion))
-
-	core := logger.Core()
-	core = zapcore.NewTee(core, sentryCore(zap.ErrorLevel))
-	logger = zap.New(core)
-
-	return logger, nil
-}
-
-func sentryCore(minLevel zapcore.Level) zapcore.Core {
-	return zapcore.RegisterHooks(zapcore.NewCore(
-		zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig()),
-		zapcore.AddSync(zapcore.AddSync(sentryLogWriter{})),
-		zap.NewAtomicLevelAt(minLevel),
-	), func(entry zapcore.Entry) error {
-		if entry.Level >= zapcore.ErrorLevel {
-			sentry.CaptureMessage(entry.Message)
+	if loggerConf.EnableFile {
+		rotationTime, err := time.ParseDuration(loggerConf.RotationTime)
+		if err != nil {
+			panic("Invalid rotation time: " + err.Error())
 		}
-		return nil
-	})
+
+		lumberjackLogger := &lumberjack.Logger{
+			Filename:   filepath.Join(loggerConf.Dir, fileName(appConf.Name, appConf.Version)),
+			MaxSize:    loggerConf.MaxSize, // megabytes
+			MaxBackups: loggerConf.MaxBackups,
+			MaxAge:     loggerConf.MaxAge, // days
+			Compress:   loggerConf.Compress,
+		}
+
+		timeRotatingWriter := NewTimeRotatingWriter(lumberjackLogger, rotationTime)
+		fileWriter := zapcore.AddSync(timeRotatingWriter)
+		cores = append(cores, zapcore.NewCore(encoder, fileWriter, zapcore.Level(loggerConf.FileLevel)))
+	}
+
+	// Объединяем выводы
+	core := zapcore.NewTee(cores...)
+
+	// Создаем логгер
+	logger := zap.New(core, zap.AddCaller(), zap.AddStacktrace(zapcore.ErrorLevel))
+
+	// Добавляем дополнительные поля
+	fields := []zap.Field{
+		zap.String("appName", appConf.Name),
+		zap.String("appVersion", appConf.Version),
+	}
+
+	logger = logger.With(fields...)
+
+	return &LogIt{logger: logger}
 }
 
-type sentryLogWriter struct{}
+// TraceId - генерация уникального идентификатора трассировки
+func (receiver *LogIt) TraceId(traceId *string) *string {
+	if traceId != nil {
+		return traceId
+	}
 
-func (s sentryLogWriter) Write(p []byte) (n int, err error) {
-	sentry.CaptureMessage(string(p))
-	return len(p), nil
+	newTraceId := uuid.New().String()
+	return &newTraceId
 }
 
-func (s sentryLogWriter) Sync() error {
-	sentry.Flush(2 * time.Second)
-	return nil
-}
-
-func TraceId() string {
-	return uuid.New().String()
+func fileName(appName, appVersion string) string {
+	currentDate := time.Now().Format("2006-01-02")
+	return fmt.Sprintf("%s_%s_%s.log", appName, appVersion, currentDate)
 }
